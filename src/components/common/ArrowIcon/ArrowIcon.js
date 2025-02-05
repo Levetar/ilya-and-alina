0261e2ee6fdf1ff9ed99f29018d64c6b2eb56f8f
vickulbakin <vickulbakin@mail.ru>
2025-02-05 19:52:35 +0300

    Отправка формы на почту

---
+++ src/components/common/ArrowIcon/ArrowIcon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const ArrowIcon = ({className}) => (
+  <svg
+    width="512"
+    height="512"
+    viewBox="0 0 512 512"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+  >
+    <path d="M93.5 174.5L256.5 337.5L419.5 174.5" stroke="black" strokeWidth="24" strokeLinecap="round" />
+  </svg>
+);
